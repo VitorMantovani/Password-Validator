e65c75583673ed826a65ef453a57701d4374e411
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-12-23 08:00:55 -0300

    refactor: sequence validator refactored

--- src/validators/SequenceValidator.ts
+++ src/validators/SequenceValidator.ts
@@ -1,3 +1,17 @@
 export class SequenceValidator {
-    
+  constructor(private asciiCodes: number[]) {}
+
+  validateCharsSequence(): boolean | string {
+    const charsSequenceCount = this.asciiCodes.reduce((acc, code, idx) => {
+        const middle = this.asciiCodes[idx + 1];
+        const last = this.asciiCodes[idx + 2];
+        const passwordThreeCharsSequence = String.fromCharCode(code, middle, last);
+        const asciiThreeCharsSequence = String.fromCharCode(code, code + 1, code + 2);
+
+        if (passwordThreeCharsSequence === asciiThreeCharsSequence) acc++;
+        return acc;
+    }, 0)
+
+    return charsSequenceCount > 0 ? "Caracteres em sequencia!" : true;
+  }
 }
